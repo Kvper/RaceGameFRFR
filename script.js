09a18a600d44fc5065aef33880b75233625c81c6
ohyeahmhmthats <90308953+Kvper@users.noreply.github.com>
2022-01-31 14:25:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+var mode;
+
+class car{
+  
+  constructor(x, y){
+  this.x = x
+  this.y = y
+  this.speed = 0
+  }
+
+start(speed) { 
+    this.speed = speed;
+  }
+
+  move() {
+    function keyPressed(){
+      if 
+    
+    }
+
+    
+    this.x += this.speed;
+    if keyPressed {
+      this.x = width;
+    } else if (this.x > width) {
+      this.x = -20;
+    }
+}
+
+function setup() {
+	createCanvas(600, 400);
+  mode = 0;
+  textSize(21);
+  desley = new car (200, 100);
+  desley.display();
+  desley.move();
+  
+}
+
+function draw(){
+ 
+  }
+  
+  
+
