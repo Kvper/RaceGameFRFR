6242b8a40e7aa2c1ba84c041de9fb7a0d3a35741
Desley Okx <90309350+Denselmentaire@users.noreply.github.com>
2022-02-07 15:08:46 +0000

    beweging

--- script.js
+++ script.js
@@ -1,92 +1,73 @@
 var player1, player2;
+var block = { x: 10, y: 10, w: 8, h: 3 }
 
 class Player {
-  constructor(x, y, vx, vy, img, up, left, right){
+  constructor(x, y, vx, vy, img, up, down, left, right, color) {
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
     this.up = up;
+    this.down = down;
     this.left = left;
     this.right = right;
-    this.color = "red";
+    this.color = color;
   }
 
-  draw(){
-    fill(this.color)
-    rect(this.x, this.y, 50, 50);
+  draw() {
+    fill("red")
+    rect(this.x, this.y, 40, 40);
+  }
+
+  keyPressed() {
+
+    if (keyIsDown(this.up)) {
+      this.y -= 4;
+    }
+
+    if (keyIsDown(this.down)) {
+      this.y += 4;
+    }
+
+    if (keyIsDown(this.left)) {
+      this.x -= 4;
+    }
+
+    if (keyIsDown(this.right)) {
+      this.x += 4;
+    }
   }
 }
 
 function setup() {
   createCanvas(500, 350);
-  player1 = new Player(10,10,5,5,"","w","a","d");
-  player2 = new Player(10,10,5,5,"",37,56,55);
+  player1 = new Player(200, 200, 5, 5, "img", 87, 83, 65, 68, "red");
+  player2 = new Player(200, 200, 5, 5, "img", 38, 40, 37, 39, "blue");
 }
-
-
-
-
-var [] = [225, 225, 0, 0];
-var [] = [225, 225, 0, 0];
-
-var block1 = { x: 100, y: 100, w: 80, h: 30}
-var block2 = { x: 100, y: 100, w: 80, h: 30}
 
 function draw() {
-  background(225);
 
+  background(225);
   fill(0);
 
-  if (xpos1 < block1.x + block1.w && xpos1 + 50 > block1.x) {
-    if (ypos1 < block1.y + block1.h && ypos1 + 50 > block1.y) {
-    }
-  }
-  if (xpos2 < block2.x + block2.w && xpos2 + 50 > block2.x) {
-    if (ypos2 < block2.y + block2.h && ypos2 + 50 > block2.y) {
-    }
-  }
+
+  // if (xpos < block.x + block.w && xpos + 50 > block.x) {
+  //   if (ypos < block.y + block.h && ypos + 50 > block.y) {
+
+  //   }
+  // }
 
   player1.draw();
-  player2.draw();  
-  
-  if (xpos1 >= 0 && xpos1 + 50 <= 500) xpos1 += xspeed1;
-  if (ypos1 >= 0 && ypos1 + 50 <= 500) ypos1 += yspeed1;
+  player1.keyPressed();
+  player2.draw();
+  player2.keyPressed();
 
-  if (xpos2 >= 0 && xpos2 + 50 <= 500) xpos2 += xspeed2;
-  if (ypos2 >= 0 && ypos2 + 50 <= 500) ypos2 += yspeed2;
+  // if (x >= 0 && x + 50 <= 500) x += vx;
+  // if (y >= 0 && y + 50 <= 500) y += vy;
+
+  // if (x >= 0 && x + 50 <= 500) x += vx;
+  // if (y >= 0 && y + 50 <= 500) y += vy;
+
 }
 
-function keyPressed() {
-  switch (keyCode) {
-    case 37:
-      xspeed1 = -2;
-      break;
-    case 39:
-      xspeed1 = 2;
-      break;
-    case 38:
-      yspeed1 = -2;
-      break;
-    case 40:
-      yspeed1 = 2;
-      break;
-  }
-}
 
-function keyReleased() {
-  switch (keyCode) {
-    case 37:
-      xspeed1 = 0;
-      break;
-    case 39:
-      xspeed1 = 0;
-      break;
-    case 38:
-      yspeed1 = 0;
-      break;
-    case 40:
-      yspeed1 = 0;
-      break;
-  }
-}
