f79c8ef9bf32c369c43a6465c972e1ae077bbe13
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2022-02-09 10:35:30 +0000

    gestart aan boandaries maken, blockclass en start aan draw

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var player1, player2;
 var block = { x: 10, y: 10, w: 8, h: 3 }
 
+class Block {
+  constructor(x, y, w, h, colour) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+  }
+
+  draw() {
+    fill("green")
+    rect(this.x, this.y, this.w, this.h)
+    
+  }
+
+}
+
+
 class Player {
   constructor(x, y, vx, vy, img, up, down, left, right, color) {
     this.x = x;
@@ -43,6 +61,7 @@ function setup() {
   createCanvas(500, 350);
   player1 = new Player(175, 36, 5, 5, "img", 87, 83, 65, 68, "red");
   player2 = new Player(175, 53, 5, 5, "img", 38, 40, 37, 39, "blue");
+  block1 = new Block(10, 500, 20, 20, "green")
   bg = loadImage('Images/bg1.png')
 }
 
@@ -62,6 +81,7 @@ function draw() {
   player1.keyPressed();
   player2.draw();
   player2.keyPressed();
+  block1.draw();
 
   // if (x >= 0 && x + 50 <= 500) x += vx;
   // if (y >= 0 && y + 50 <= 500) y += vy;
